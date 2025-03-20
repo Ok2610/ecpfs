f6cf41ddaec109e54cad7246e3e7ca77af57f8df
Omar Khan <ok2610@gmail.com>
2025-03-20 11:45:21 +0000

    Formatting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import Annotated
 from pathlib import Path
 
 from ecpfs import ECPBuilder
+from ecpfs.ECPBuilder import Metric
 
 app = typer.Typer()
 logger.remove()  # Remove default logging handler
@@ -14,28 +15,21 @@ logger.add(
 )
 
 
-@app.command('build-index')
+@app.command("build-index")
 def build_index(
     embeddings_file: Annotated[
         Path,
-        typer.Argument(
-            help="Embeddings file with data vectors. Zarr or HDF5 file"
-        ),
+        typer.Argument(help="Embeddings file with data vectors. Zarr or HDF5 file"),
     ],
     save_file: Annotated[
-        str,
-        typer.Option(
-            '--save-file',
-            help="Output file"
-        )
+        str, typer.Option("--save-file", help="Output file")
     ] = "ecpfs_index.zarr",
     levels: Annotated[int, typer.Option(help="Levels in the index")] = 3,
     target_cluster_size: Annotated[
         int,
         typer.Option(
-            '--target-cluster-size',
-            help="Preferred size of clusters (no guarantees)"
-        )
+            "--target-cluster-size", help="Preferred size of clusters (no guarantees)"
+        ),
     ] = 100,
     metric: Annotated[
         str,
@@ -43,7 +37,7 @@ def build_index(
             help="Metric to use for distance calculations. Options: L2 (default) | IP | cos",
             case_sensitive=False,
             metavar="FORMAT",
-            show_choices=["L2", "IP", "cos"]
+            show_choices=["L2", "IP", "cos"],
         ),
     ] = "L2",
     workers: Annotated[
@@ -59,10 +53,7 @@ def build_index(
     ] = False,
     emb_grp_name: Annotated[
         str,
-        typer.Option(
-            "--emb-grp-name",
-            help="Group name for the embeddings dataset"
-        ),
+        typer.Option("--emb-grp-name", help="Group name for the embeddings dataset"),
     ] = "embeddings",
     file_store: Annotated[
         str,
@@ -71,7 +62,7 @@ def build_index(
             help="File store format. Options are 'zarr_l' (zarr.storage.LocalStore), 'zarr_z' (zarr.storage.ZipStore), 'h5' (HDF5)",
             case_sensitive=False,
             metavar="FORMAT",
-            show_choices=["zarr_l", "zarr_z", "h5"]
+            show_choices=["zarr_l", "zarr_z", "h5"],
         ),
     ] = "zarr_l",
     rep_selection: Annotated[
@@ -81,29 +72,22 @@ def build_index(
             help="Parameter for how the representatives are selected. Options are 'offset', 'random', 'mbk' (MiniBatchKmeans) or 'dissimilar' (Not implemented)",
             case_sensitive=False,
             metavar="REPSEL",
-            show_choices=["offset", "random", "mbk", "dissimilar"]
-        )
+            show_choices=["offset", "random", "mbk", "dissimilar"],
+        ),
     ] = "offset",
     rep_file: Annotated[
         Path,
         typer.Option(
             "--rep-file",
-            help="A zarr or HDF5 file containing pre-selected representatives"
-        )
+            help="A zarr or HDF5 file containing pre-selected representatives",
+        ),
     ] = None,
     rep_emb_grp: Annotated[
         str,
-        typer.Option(
-            "--rep-emb-grp",
-            help="Representative file embeddings group name"
-        )
+        typer.Option("--rep-emb-grp", help="Representative file embeddings group name"),
     ] = "clst_embeddings",
     rep_ids_grp: Annotated[
-        str,
-        typer.Option(
-            "--rep-ids-grp",
-            help="Representative file ids group name"
-        )
+        str, typer.Option("--rep-ids-grp", help="Representative file ids group name")
     ] = "clst_item_ids",
     rep_file_store: Annotated[
         str,
@@ -112,7 +96,7 @@ def build_index(
             help="Represenatives file store format. Options are 'zarr_l' (zarr.storage.LocalStore), 'zarr_z' (zarr.storage.ZipStore), 'h5' (HDF5)",
             case_sensitive=False,
             metavar="FORMAT",
-            show_choices=["zarr_l", "zarr_z", "h5"]
+            show_choices=["zarr_l", "zarr_z", "h5"],
         ),
     ] = "zarr_l",
 ) -> None:
@@ -146,13 +130,14 @@ def build_index(
             embeddings_file=embeddings_file,
             grp=False if no_emb_grp else True,
             grp_name=emb_grp_name,
-            option=rep_selection
+            option=rep_selection,
         )
     else:
         ecp.get_cluster_representatives_from_file(
-            rep_file, emb_dsname=rep_emb_grp,
-            ids_dsname=rep_ids_grp, 
-            format=rep_file_store
+            rep_file,
+            emb_dsname=rep_emb_grp,
+            ids_dsname=rep_ids_grp,
+            format=rep_file_store,
         )
 
     logger.info("Building tree...")
